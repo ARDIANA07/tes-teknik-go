d4a7512e68b31f38ab9de0c6addd1d66fbdc47e1
Ardiana Abdul Gumelar <aridianaabdul@gmail.com>
2023-05-21 09:31:43 +0700

    update entity

--- ingredient/entity.go
+++ ingredient/entity.go
@@ -1,6 +1,7 @@
 package ingredient
 
 type Ingredient struct {
-	ID   string `json:"id"`
-	Name string `json:"name"`
+	ID          string `json:"id"`
+	Name        string `json:"name" type:"varchar(255)"`
+	JumlahBahan string `json:"jumlah_bahan" type:"varchar(20)"`
 }
--- recipe/entity.go
+++ recipe/entity.go
@@ -2,7 +2,8 @@ package recipe
 
 type Recipe struct {
 	ID          string   `json:"id"`
-	Name        string   `json:"name"`
+	Name        string   `json:"title" type:"varchar(50)"`
+	Description string   `json:"description" type:"text"`
 	Ingredients []string `json:"ingredients"`
 	Category    string   `json:"category"`
 }
